eb9b06d8aad68019bc9583d523179ff0c1c44dbf
Timbre Freeman <tfreeman3@ewu.edu>
2021-11-07 16:45:32 -0800

    Initial commit

---
+++ src/test/java/tech/tftinker/helloworld/TestMain.java
@@ -0,0 +1,31 @@
+package tech.tftinker.helloworld;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMain {
+    @Test
+    public void testOutputOfMain() {
+        String outputText = "Hello World!\n";
+
+        PrintStream outputStream = System.out;
+
+        ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+
+        Main.main(new String[0]);
+
+        assertEquals(outputText, myOut.toString().replace("\r",""));
+
+        System.setOut(outputStream);
+    }
+
+    @Test
+    public void thisTestWillFail() {
+        //fail("This test will Fail");
+    }
+}
